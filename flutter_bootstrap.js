feea6a4003872c253c50ac322d70eac5a660ef42
onlytushar <ghigetushar@gmail.com>
2025-10-19 21:58:41 +0530

    title and description changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "572116060"
+    serviceWorkerVersion: "2327827641"
   }
 });
